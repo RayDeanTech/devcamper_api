7308babc98b639fbb5b579250d1ccd817ce21587
Raymond Dean <RayDeanTech@outlook.com>
2023-07-15 11:41:35 -0400

    experiment w/logger middleware & installed Morgan

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // route files
 const bootcamps = require('./routes/bootcamps');
 
-//load env vars
+// load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
+
 
 // mount routers
 app.use('/api/v1/bootcamps', bootcamps);
