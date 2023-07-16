ee63e742bf0923acb45a867729bd97b7b97dd8b3
Raymond Dean <RayDeanTech@outlook.com>
2023-07-16 16:11:47 -0400

    handled GET Bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,25 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: "Show all bootcamps" });
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({ success: true, data: bootcamps });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    };
 };
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Show a single bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 };
 
 // @desc    Create new bootcamp
@@ -19,8 +29,12 @@ exports.getBootcamp = (req, res, next) => {
 // @access  Private
 exports.createBootcamp = async (req, res, next) => {
     try {
-
         const bootcamp = await Bootcamp.create(req.body);
+
+        // correctly formated, but does not exist
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
+        };
 
         res.status(201).json({
             success: true,
@@ -30,9 +44,7 @@ exports.createBootcamp = async (req, res, next) => {
     } catch (err) {
 
         res.status(400).json({ success: false });
-
     };
-
 };
 
 // @desc    Update bootcamp
