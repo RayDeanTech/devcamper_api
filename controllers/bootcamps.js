d8436a7282ac407c5d2569fd06d0fe018ca510ea
Raymond Dean <RayDeanTech@outlook.com>
2023-09-30 18:33:51 -0400

    Custom ErrorResponse Class
    We can now instantiate a custom error response in the bootcamps.js controller.  Additionally, since the Error class was extended, we can use its new statusCode in the custom error handler middleware as well.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResponse')
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
@@ -18,10 +19,17 @@ exports.getBootcamps = async (req, res, next) => {
 exports.getBootcamp = async (req, res, next) => {
     try {
         const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            return next(
+                new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+            );
+        };
+
         res.status(200).json({ success: true, data: bootcamp });
     } catch (err) {
         // res.status(400).json({ success: false });
-        next(err);
+        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
 };
 
