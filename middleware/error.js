c9011e77cc55e15761bba2afc9492cbed0d17c61
Raymond Dean <RayDeanTech@outlook.com>
2023-09-30 18:07:30 -0400

    Error Handler Middleware

---
+++ middleware/error.js
@@ -0,0 +1,13 @@
+// https://expressjs.com/en/guide/error-handling.html
+
+const errorHandler = (err, req, res, next) => {
+    // log to console for dev
+    console.log(err.stack.red);
+
+    res.status(500).json({
+        success: false,
+        error: err.message
+    });
+};
+
+module.exports = errorHandler;
